438b162db03746f31c9dd10ab7a1520b734d04e3
Adrien Husson <pangel.neu@gmail.com>
2009-03-21 02:03:39 -0700

    Time selector included. Need to work on the controller side and the design

--- application.rb
+++ application.rb
@@ -14,6 +14,25 @@ end
  
 helpers do
   # add your helpers here
+  class Fixnum    
+    # Returns a string of the integer prefixed with zeros. 
+    # Enough zeros will be added so as to make a string that is _characters_ long.
+    def width(characters)
+      "0"*(characters - self.to_s.size) + self.to_s
+    end
+  end
+
+  def times_of_day
+    times = Proc.new { |suffix| 
+        [12].concat((1..11).to_a).map! { |e| 
+          [0,15,30,45].map { |f|
+            "#{e}:#{f.width(2)} #{suffix}" 
+          }
+        }.flatten!
+      }
+  
+    times.call("am").concat(times.call("pm"))
+  end
 end
  
 get '/' do
