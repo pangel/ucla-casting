accee17a5b55687e8ad259cce514e548fc122e55
Adrien Husson <pangel.neu@gmail.com>
2009-04-03 23:38:54 -0700

    Added checksum column and appropriate migration

--- devtools.rb
+++ devtools.rb
@@ -1,12 +1,29 @@
 class Devtools
   def self.load_dev_data
     myrevenge = Audition.new
-    myrevenge.attributes = {:title => "My Revenge", :when => Time.local(2009, 04, 01, 13, 30, 00), :where => "Soundstage 2", :description => "Head must be bald or shaved.\nMust have an interesting voice (you can attach a sound sample).\n\nThe character is mostly a voiceover but throughout the film his silhouette will appear."}
+    myrevenge.attributes = {:title => "My Revenge", :when => Time.local(2009, 04, 15, 13, 30, 00), :where => "Soundstage 2", :description => "Head must be bald or shaved.\nMust have an interesting voice (you can attach a sound sample).\n\nThe character is mostly a voiceover but throughout the film his silhouette will appear."}
     myrevenge.save
 
     thebox = Audition.new
-    thebox.attributes = {:title => "The Box", :when => Time.local(2009, 04, 03, 15, 15, 00), :where => "Soundstage 1", :description => "The character is homeless but this status is only revealed at the end - he or she looks very poor.\n
+    thebox.attributes = {:title => "The Box", :when => Time.local(2009, 06, 03, 15, 15, 00), :where => "Soundstage 1", :description => "The character is homeless but this status is only revealed at the end - he or she looks very poor.\n
 Wardrobe: USED CLOTHES - CAN BE PROVIDED."}
     thebox.save
   end
+  
+  module Migrations
+    class << self
+      def add_checksums
+        unchecked_auditions = Audition.all(:sha1 => nil)
+        unchecked_auditions.each { |a|
+          a.sha1 = Audition.checksum(a)
+          a.save
+        }
+      end
+    end
+  end
+  
+  def self.migrations
+    Migrations
+  end    
+  
 end
--- lib/models/audition.rb
+++ lib/models/audition.rb
@@ -6,6 +6,14 @@ class Audition
   property :when, Time
   property :where, String
   property :description, Text
- 
+  property :sha1, String, :length => 128, :default => Proc.new { |r, p| checksum(r)}
+  
+  def self.checksum(r)
+    require 'sha1'
+    blob = r.title + r.when.to_s + r.where + r.description
+    SHA1.new blob
+  end
+  
   # validates_present :name
 end
+
